e571f404ecf0a2802e68a0b856a418c4413aec21
objcat <objcat@163.com>
2021-01-29 11:36:42 +0800

    打印和读取异步

--- src/main/java/com/objcat/playground/socket/SocketServer.java
+++ src/main/java/com/objcat/playground/socket/SocketServer.java
@@ -1,9 +1,6 @@
 package com.objcat.playground.socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,12 +9,100 @@ public class SocketServer {
 
     public static void main(String[] args) {
         SocketServer socketServer = new SocketServer();
-        socketServer.start2();
+        socketServer.start3();
     }
 
-    /**
-     * 异步客户端
-     */
+
+    public void start3() {
+        try {
+            ServerSocket server = null;
+            try {
+                //创建一个ServerSocket在端口5209监听客户请求
+                server = new ServerSocket(this.port);
+                System.out.println("服务器启动成功 127.0.0.1:" + this.port);
+            } catch (Exception e) {
+                System.out.println("没有启动监听：" + e);
+            }
+            Socket socket = null;
+
+            try {
+                //2、调用accept()方法开始监听，等待客户端的连接
+                //使用accept()阻塞等待客户请求，有客户
+                //请求到来则产生一个Socket对象，并继续执行
+                socket = server.accept();
+            } catch (Exception e) {
+                System.out.println("Error." + e);
+            }
+
+            //由Socket对象得到输入流，并构造相应的BufferedReader对象
+            final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            final BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+
+            Thread readThread = new Thread() {
+                public void run() {
+                    while (true) {
+                        try {
+                            String line = in.readLine();
+                            System.out.println(line);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            try {
+                                in.close();
+                            } catch (IOException ioException) {
+                                ioException.printStackTrace();
+                            }
+                        }
+
+                    }
+                }
+            };
+
+            Thread writeThread = new Thread() {
+                public void run() {
+                    while (true) {
+                        String message = null;
+                        try {
+                            message = sysin.readLine();
+                            System.out.println("发送消息:" + message);
+                            out.write(message);
+                            out.flush();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            try {
+                                out.close();
+                            } catch (IOException ioException) {
+                                ioException.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            };
+
+
+            readThread.start();
+            writeThread.start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ServerSocket serverSocket, Socket socket, BufferedReader reader, BufferedWriter writer) {
+        try {
+            serverSocket.close();
+            socket.close();
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     public void start2() {
         try {
             ServerSocket server = null;
@@ -44,6 +129,14 @@ public class SocketServer {
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String line = in.readLine();
                 System.out.println(line);
+
+//                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+//                BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+//                String line2 = sysin.readLine();
+//                if (!line2.equals("bye")) {
+//                    out.write(line2);
+//                    out.flush();
+//                }
             }
 
             //3、获取输入流，并读取客户端信息
